dba853ccf7d263e6579d3f8118ce9ac61273f071
SunnSta <mengmengzjxs@163.com>
2018-05-29 11:42:15 +0800

    1.3
    add sth

---
+++ Project/Project/Assets/StartMenu/Script/StartAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class StartAnim : MonoBehaviour {
+    public Animator Camera1;
+    public Animator Mouse1;
+    public Animator Mouse2;
+
+    // Use this for initialization
+    void Start()
+    {
+        Mouse1.enabled = true;
+        Mouse2.StopPlayback();
+        Camera1.enabled = true;
+        StartCoroutine("MyEvent");
+    }
+    // Update is called once per frame
+    void Update () {
+    }
+    private IEnumerator MyEvent()
+    {
+        yield return new WaitForSeconds(1f);
+        Mouse2.Play("exit");
+    }
+}
