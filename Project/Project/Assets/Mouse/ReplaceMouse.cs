08b64ca4eedc15da233df16c66b4441d7ecb2ced
SunnSta <mengmengzjxs@163.com>
2018-05-29 20:51:27 +0800

    Sunn 0.1
    add 开场界面

---
+++ Project/Project/Assets/Mouse/ReplaceMouse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceMouse : MonoBehaviour
+{
+    public Texture mouse;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Cursor.visible = false;//隐藏鼠标指针
+    }
+
+    void OnGUI()
+    {
+        Vector2 msPos=Input.mousePosition;//鼠标的位置
+        GUI.DrawTexture(new Rect(msPos.x, Screen.height-msPos.y, mouse.width, mouse.height), mouse);
+    }
+
+}
