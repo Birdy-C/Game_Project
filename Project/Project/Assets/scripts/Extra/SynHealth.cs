fc7cd79c912f1bf40783b98a3d5cad7914b831c9
Birdy-C <xiaoyuan2425@126.com>
2018-07-09 08:27:58 +0800

    add readme

---
+++ Project/Project/Assets/scripts/Extra/SynHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SynHealth : MonoBehaviour {
+    public Slider m_Slider_from;
+    public Slider m_Slider_to;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_Slider_to.value = m_Slider_from.value;
+    }
+}
--- Project/Project/Assets/scripts/Tank/TankHealth.cs
+++ Project/Project/Assets/scripts/Tank/TankHealth.cs
@@ -6,8 +6,8 @@ using UnityEngine.SceneManagement;
 public class TankHealth : MonoBehaviour
 {
     public float m_StartingHealth = 100f;               // The amount of health each tank starts with.
-    public Slider  m_Slider;                             // The slider to represent how much health the tank currently has.
-    
+    public Slider m_Slider;                             // The slider to represent how much health the tank currently has.
+
     public Image m_FillImage;                           // The image component of the slider.
     public Color m_FullHealthColor = Color.green;       // The color the health bar will be when on full health.
     public Color m_ZeroHealthColor = Color.red;         // The color the health bar will be when on no health.
@@ -64,7 +64,6 @@ public class TankHealth : MonoBehaviour
     {
         // Set the slider's value appropriately.
         m_Slider.value = m_CurrentHealth;
-
         // Interpolate the color of the bar between the choosen colours based on the current percentage of the starting health.
         m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
     }
@@ -95,12 +94,12 @@ public class TankHealth : MonoBehaviour
         return m_Dead;
     }
 
-    IEnumerator LoadScene(string sceneName)  
-    {  
-        AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);  
-        yield return new WaitForEndOfFrame();  
-        op.allowSceneActivation = true;  
-  
+    IEnumerator LoadScene(string sceneName)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneName);
+        yield return new WaitForEndOfFrame();
+        op.allowSceneActivation = true;
+
     }
 
 }
